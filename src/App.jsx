4d8c60cf4d1b3fc48f370ca5b09b1365dbe44e9b
Vivekanand P V <vivekanandpv@outlook.com>
2023-10-29 11:54:41 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = (props) => {
+  return (
+    <>
+      <div className='p-5'>
+        <h2>React Training App</h2>
+        <hr />
+      </div>
+    </>
+  );
+};
+
+export default App;
